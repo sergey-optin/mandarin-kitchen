8b6c3e3581cd28d7ab0912325582432545be49ae
Sergey Sherstobitov <sergeys@opt-inmedia.com>
2019-12-19 17:00:08 -0800

    Work on shopping cart buttons.

--- assets/js/shoppingCart.js
+++ assets/js/shoppingCart.js
@@ -2,3 +2,8 @@ function openShoppingCart() {
     let shoppingCartContainer = document.getElementById("shopping-cart-container");
     shoppingCartContainer.style.display = "flex";
 }
+
+function closeShoppingCart() {
+    let shoppingCartContainer = document.getElementById("shopping-cart-container");
+    shoppingCartContainer.style.display = "none";
+}
